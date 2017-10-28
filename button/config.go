13365a030f9b04d5d50e80f8de5af16eb6b4c6f2
Massimo Mangoni <massimo@mangoni.org>
2017-10-28 18:24:27 +0200

    Integrated audio driver to play sounds on events (button presses). Configuration with spf13/viper.

---
+++ button/config.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/spf13/viper"
+
+type applicationConfig struct {
+	Button struct {
+		Pin            string
+		PollInterval   int
+		DefaultState   int
+		PressSoundFile string
+	}
+}
+
+var Config applicationConfig
+
+func LoadConfig() error {
+	v := viper.New()
+	v.SetConfigName("config")
+	v.AddConfigPath("/etc/godot")
+	v.AddConfigPath(".")
+	if err := v.ReadInConfig(); err != nil {
+		return err
+	}
+	if err := v.Unmarshal(&Config); err != nil {
+		return err
+	}
+	return nil
+}
--- button/godot.go
+++ button/godot.go
@@ -1,33 +1,43 @@
 package main
 
 import (
-	"fmt"
-
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/audio"
 	"gobot.io/x/gobot/platforms/raspi"
+
+	"log"
 
 	"time"
 )
 
 func main() {
+	log.Println("Starting Godot")
+	if err := LoadConfig(); err != nil {
+		log.Fatal("Failed loading configuration file: ", err)
+	}
+
 	r := raspi.NewAdaptor()
-	button := gpio.NewButtonDriver(r, "7", 100*time.Millisecond)
-	button.DefaultState = 1
+	button := gpio.NewButtonDriver(r, Config.Button.Pin, time.Duration(Config.Button.PollInterval)*time.Millisecond)
+	button.DefaultState = Config.Button.DefaultState
+
+	a := audio.NewAdaptor()
+	ring := audio.NewDriver(a, Config.Button.PressSoundFile)
 
 	work := func() {
 		button.On(gpio.ButtonPush, func(data interface{}) {
-			fmt.Println("button pressed")
+			log.Println("ring button pressed")
+			ring.Play()
 		})
 
 		button.On(gpio.ButtonRelease, func(data interface{}) {
-			fmt.Println("button released")
+			log.Println("ring button released")
 		})
 	}
 
-	robot := gobot.NewRobot("buttonBot",
-		[]gobot.Connection{r},
-		[]gobot.Device{button},
+	robot := gobot.NewRobot("Godot Robot",
+		[]gobot.Connection{r, a},
+		[]gobot.Device{button, ring},
 		work,
 	)
 
